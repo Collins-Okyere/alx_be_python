64e56e9d89cefef3dde8b2bec4f58c122e57480f
Collins <102311172+Collins-Okyere@users.noreply.github.com>
2025-01-09 23:48:56 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
-def perform_operation(num1, num2, operation):
-    if operation == 'add':
-        result = num1 + num2
-    elif operation == 'subtract':
-        result = num1 - num2
-    elif operation == 'multiply':
-        result = num1 * num2
-    elif operation == 'divide':
-        if num1 == 0:
-            return "Zero is not divisible."
-    else:
-        return "Invalid operation. Please choose 'add', 'subtract', 'multiply', or 'divide'."
+def perform_operation(num1: float, num2: float, operation: str):
+    match operation:
+        case 'add':
+            result = num1 + num2
+        case 'subtract':
+            result = num1 - num2
+        case 'multiply':
+            result = num1 * num2
+        case 'divide':
+            if num1 == 0:
+                return "Zero is not divisible."
+        case _:
+            return "Invalid operation. Please choose 'add', 'subtract', 'multiply', or 'divide'."
     
     return result
