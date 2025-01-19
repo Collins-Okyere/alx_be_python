99bfea6ef63ea7c7c271a877ece70f0cd0364f08
Collins <102311172+Collins-Okyere@users.noreply.github.com>
2025-01-19 23:08:37 +0000

    Update test_simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,8 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    """Unit test for us the SimpleCalculator class."""
+
+    def setUp(self):
+        """
